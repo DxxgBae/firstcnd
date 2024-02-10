085d9de6fbb5d6e2506063fcf21d2f3f1888f926
SonDongBae <dxxgbae@gmail.com>
2024-02-11 04:50:42 +0900

    swipe ing

--- src/js/History.js
+++ src/js/History.js
@@ -1,19 +1,39 @@
+import { useState, useEffect } from 'react';
 import dataHistory from '../data/history.json';
 import SectionTitle from './SectionTitle'
+import Swipe from './Swipe';
 import '../css/History.css';
 
 function History() {
+    const [isPortrait, setIsPortrait] = useState(false);
+
+    useEffect(() => {
+        const handleOrientationChange = (e) => {
+            setIsPortrait(e.matches);
+        };
+
+        const mediaQuery = window.matchMedia('(orientation: portrait)');
+        handleOrientationChange(mediaQuery);
+        mediaQuery.addEventListener('change', handleOrientationChange);
+        return () => mediaQuery.removeEventListener('change', handleOrientationChange);
+    }, [isPortrait]);
+
     return (
         <section id='History'>
             <div className='noise' />
             <SectionTitle h='회사연혁' p={['퍼스트씨엔디가 걸어온 길을 소개드립니다.']} />
-            <div className='container'>
+            <Swipe
+                perView={isPortrait ? 1 : 3}
+                loop={true}
+            >
+                {!isPortrait && <div className='item' />}
                 {dataHistory.map((item, index) => (
-                    <div key={index} className='year'>
+                    <div key={index} className='item'>
+                        <i className='fa-solid fa-diamond' />
                         <h2 className='handfont'>
                             {item.yyyy}
                         </h2>
-                        <div className='items'>
+                        <div className='year'>
                             {item.items.map((item2, index2) => (
                                 <div key={index2} className='month'>
                                     <h5>
@@ -32,8 +52,8 @@ function History() {
                         </div>
                     </div>
                 ))}
-            </div>
-        </section>
+            </Swipe>
+        </section >
     );
 }
 
